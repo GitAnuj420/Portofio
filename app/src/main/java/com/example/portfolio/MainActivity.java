3ad85e02cf5cb95f46ac9575fa5a00b0a22919f9
GitAnuj420 <101204714+GitAnuj420@users.noreply.github.com>
2022-09-10 13:11:11 +0530

    First added

---
+++ app/src/main/java/com/example/portfolio/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.portfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void openGit(View view) {
+        Toast.makeText(MainActivity.this, "Openning Git...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void form(View view) {
+        Toast.makeText(MainActivity.this, "Add another profile...", Toast.LENGTH_SHORT).show();
+    }
+}
